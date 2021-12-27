1be0eb17deb4e85a6a6eab1fd7187f6d2a9cf36a
Prokhor <32173558+prokhororlov@users.noreply.github.com>
2021-12-27 09:02:21 +0300

    feat: add auth (#3)

--- frontend/src/components/organisms/Header.vue
+++ frontend/src/components/organisms/Header.vue
@@ -1,15 +1,34 @@
 <template>
-  <el-menu class="header" mode="horizontal"
-    :default-active="activeIndex"
-    @click="onClickMenuItem">
-    <el-menu-item index="0"><router-link to="/">Home</router-link></el-menu-item>
-    <el-menu-item index="1"><router-link to="/stocks/">Stocks</router-link></el-menu-item>
-  </el-menu>
+  <div class="header">
+    <el-menu mode="horizontal"
+      :default-active="activeIndex"
+      @click="onClickMenuItem">
+      <el-menu-item index="0"><router-link to="/">Home</router-link></el-menu-item>
+      <el-menu-item index="1"><router-link to="/stocks/">Stocks</router-link></el-menu-item>
+    </el-menu>
+    <div class="header__right">
+      <template v-if="$auth.authenticated">
+        <el-button @click="logout">LogOut</el-button>
+        <el-avatar shape="circle" :size="32" :src="$auth.user?.picture" />
+      </template>
+      <template v-else>
+        <el-button @click="login">LogIn</el-button>
+      </template>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
   methods: {
+    login() {
+      this.$auth.loginWithPopup()
+        .then(console.log);
+    },
+    logout() {
+      this.$auth.logout();
+      this.$router.push({ path: '/' });
+    },
     onClickMenuItem(e) {
       const target = e.path[0];
       if (target.tagName === 'LI') {
@@ -31,6 +50,23 @@ export default {
 
 <style lang="scss">
 .header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  border-bottom: solid 1px #e6e6e6;
+  padding: 0 16px;
+
+  &__right {
+    display: grid;
+    grid-auto-flow: column;
+    grid-gap: 16px;
+    align-items: center;
+  }
+
+  .el-menu.el-menu--horizontal {
+    border: none;
+  }
+
   .el-menu-item {
     position: relative;
   }
