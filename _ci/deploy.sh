aa702be997e72c4ce0de5a146ab9be9533f5fdcb
Прохор <prokhororlov@gmail.com>
2023-10-18 16:39:09 +0700

    ci: fix deploy

--- _ci/deploy.sh
+++ _ci/deploy.sh
@@ -3,7 +3,10 @@ pm2 stop index
 # rm -rf node_modules
 # rm -rf backend/node_modules
 # rm -rf frontend/node_modules
+# node_v=$(node -v) # save current node version
+# nvm use 14
 yarn && yarn build
 cd backend
 pm2 start index.js
 cd ..
+# nvm use $node_v # return node version used before
