6a3b734e720deb89c14d07441d31cf43da425ecd
Прохор <prokhororlov@gmail.com>
2023-10-18 16:15:00 +0700

    ci: add deploy script

--- _ci/deploy.sh
+++ _ci/deploy.sh
@@ -5,5 +5,7 @@ rm --rf frontend/node_modules
 node_v=$(node -v) # save current node version
 nvm use 14
 yarn && yarn build
-cd backend && pm2 start index.js cd ..
+cd backend
+pm2 start index.js
+cd ..
 nvm use $node_v # return node version used before
