bdb5584fca9f2a4bca47671802617610ddb63149
Прохор <prokhororlov@gmail.com>
2023-10-18 16:17:57 +0700

    ci: add deploy script

--- _ci/deploy.sh
+++ _ci/deploy.sh
@@ -1,11 +1,12 @@
+git pull origin main
 pm2 stop index
-rm --rf node_modules
-rm --rf backend/node_modules
-rm --rf frontend/node_modules
+# rm -rf node_modules
+# rm -rf backend/node_modules
+# rm -rf frontend/node_modules
 node_v=$(node -v) # save current node version
-nvm use 14
+npm nvm use 14
 yarn && yarn build
 cd backend
 pm2 start index.js
 cd ..
-nvm use $node_v # return node version used before
+npm nvm use $node_v # return node version used before
